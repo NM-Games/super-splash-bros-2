67838f384cf33bdc5a36f386c3a82a41096018fb
ILoveAndLikePizza <nathankoning@gmail.com>
2024-05-29 21:31:57 +0200

    Fix ping text flashing red

--- app/preload/index.js
+++ app/preload/index.js
@@ -1529,7 +1529,7 @@ addEventListener("DOMContentLoaded", () => {
             const text = (game.remaining >= 0) ? `Water starts rising in ${m}:${s}` : (game.flooded) ? "Fight to the victory!" : "Water is rising!";
             const color = (game.remaining < 0 && !game.flooded && frames % 60 < 30) ? theme.colors.error.foreground : theme.getTextColor();
             c.draw.text({text, x: 15, y: 35, color, font: {size: 28}, alignment: "left"});
-            if (state.current === state.PLAYING_LAN) c.draw.text({text: `Ping: ${ping} ms`, x: c.width() - 15, y: 25, color, font: {size: 12}, alignment: "right"});
+            if (state.current === state.PLAYING_LAN) c.draw.text({text: `Ping: ${ping} ms`, x: c.width() - 15, y: 25, font: {size: 12}, alignment: "right"});
         } else drawWater();
 
 
